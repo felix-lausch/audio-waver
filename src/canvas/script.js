980d09c7fd68d6d4ae8603fc7f8b414039631673
Felix Lausch <felix.lausch@justin-legal.com>
2025-06-18 16:47:56 +0200

    add basic fractal canvas drawing

--- src/canvas/script.js
+++ src/canvas/script.js
@@ -1,67 +1,130 @@
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
-  canvas.width = this.window.innerWidth * 0.8;
-  canvas.height = this.window.innerHeight * 0.8;
+  canvas.width = this.window.innerWidth;
+  canvas.height = this.window.innerHeight;
 
   ctx.fillStyle = "green";
-  // ctx.fillRect(50, 50, 100, 100);
-  ctx.strokeStyle = "yellow";
-  ctx.lineWidth = 10;
+  ctx.lineWidth = 20;
   ctx.lineCap = "round";
+  ctx.shadowColor = 'rgba(0,0,0,0.9)';
+  ctx.shadowOffsetX = 1;
+  ctx.shadowOffsetY = 5;
+  ctx.shadowBlur = 10;
 
-  let size = 100;
-  let sides = 9;
-  let maxCount = 10;
-  let spread = 1.4;
+  //fractal variables
+  let size = canvas.width < canvas.height ? canvas.width * 0.3 : canvas.height * 0.3;
+  const maxLevel = 3;
+  const branches = 2;
+  let sides = 5;
+  let scale = 0.5;
+  let spread = 0.9;
+  let color = 'hsl(' + Math.random() * 360 + ', 100%, 50%)';
 
-  ctx.save();
-  ctx.translate(canvas.width/2, canvas.height/2);
-  ctx.scale(1, 1);
-  ctx.rotate(0);
+  //controls
+  const randomizeButton = this.document.getElementById("randomizeButton");
+  const resetButton = this.document.getElementById("resetButton");
 
-  function drawBranch(count) {
-    if (count > 4) {
-      console.log("drew branchas")
-      return
-    };
+  const sidesSlider = this.document.getElementById("sides");
+  const sidesLabel = this.document.querySelector('[htmlFor="sides"]');
+  
+  const spreadSlider = this.document.getElementById("spread");
+  const spreadLabel = this.document.querySelector('[htmlFor="spread"]');
+
+  function drawBranch(level) {
+    if (level > maxLevel) return
 
     ctx.beginPath();
     ctx.moveTo(0,0);
     ctx.lineTo(size, 0);
     ctx.stroke();
 
+    for (let i = 0; i < branches; i++) {
+      ctx.save();
+      ctx.translate(size - (size/branches) * i, 0);
+      ctx.rotate(spread);
+      ctx.scale(scale , scale);
+      drawBranch(level + 1);
+      ctx.restore();
+
+      ctx.save();
+      ctx.translate(size - (size/branches) * i, 0);
+      ctx.rotate(-spread);
+      ctx.scale(scale , scale);
+      drawBranch(level + 1);
+      ctx.restore();
+    }
+  }
+
+
+  function drawFractal() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
-    ctx.translate(size/2.0, size/2.0);
-    ctx.rotate(spread);
-    ctx.scale(0.8, 0.8);
-    drawBranch(count + 1);
-    ctx.restore();
-    
-    ctx.save();
-    ctx.translate(size/2.0, size/2.0);
-    ctx.rotate(-spread);
-    ctx.scale(0.8, 0.8);
-    drawBranch(count + 1);
+    ctx.strokeStyle = color;
+
+    ctx.translate(canvas.width/2, canvas.height/2);
+
+    for(let i = 0; i < sides; i++) {
+      ctx.rotate((Math.PI * 2)/sides);
+      drawBranch(0);
+    }
+
     ctx.restore();
   }
 
-  drawBranch(0);
-  // for (let i = 0; i < sides; i++) {
-  //   drawBranch(0);
-  //   ctx.scale(0.80, 0.80);
-  //   ctx.translate(30, 50);
-  // }
+  drawFractal();
 
+  randomizeButton.addEventListener("click", () => {
+    randomizeFractal();
+    drawFractal();
+  })
 
-  // ctx.beginPath();
-  // ctx.moveTo(0,0);
-  // ctx.lineTo(size, 0);
-  // ctx.stroke();
-  
-  // ctx.restore();
+  resetButton.addEventListener("click", () => {
+    resetFractal()
+    drawFractal()
+  })
+
+  sidesSlider.addEventListener("change", (e) => {
+    sides = e.target.value
+    updateSliders()
+    drawFractal()
+  })
+
+  spreadSlider.addEventListener("change", (e) => {
+    spread = e.target.value
+    updateSliders()
+    drawFractal()
+  })
+
+  function randomizeFractal() {
+    sides = Math.floor(Math.random() * 7 + 2);
+    scale = Math.random() * 0.2 + 0.4;
+    spread = Math.random() * 2.9 + 0.1;
+    color = 'hsl(' + Math.random() * 360 + ', 100%, 50%)';
+    updateSliders()
+  }
+
+  function resetFractal() {
+    sides = 5;
+    scale = 0.5;
+    spread = 0.7;
+    color = 'hsl(290, 100%, 50%)';
+    updateSliders()
+  }
+
+  function updateSliders() {
+    sidesLabel.innerText = 'Sides: ' + Number(sides).toFixed(1)
+    sidesSlider.value = sides;
+
+    spreadLabel.innerText = 'Spread: ' + Number(spread).toFixed(2)
+    spreadSlider.value = spread;
+  }
+
+  window.addEventListener("resize", () => {
+    // canvas.width = window.innerWidth;
+    // canvas.height = window.innerHeight;
+    // size = window.innerWidth < window.innerHeight ? window.innerWidth * 0.3 : window.innerHeight * 0.3;
+    // drawFractal()
+  })
 });
 
-// function delay(ms) {
-//   return new Promise(resolve => setTimeout(resolve, ms));
-// }
